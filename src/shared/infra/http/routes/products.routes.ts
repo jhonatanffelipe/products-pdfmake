61c6d6d1f72f94269a258fd8cf36553b4a3530db
jhonatanffelipe <jhonatanfnm@gmail.com>
2022-07-13 06:52:53 -0300

    Continuação desenvolvimento relatório com PDF Make

--- src/shared/infra/http/routes/products.routes.ts
+++ src/shared/infra/http/routes/products.routes.ts
@@ -1,5 +1,8 @@
-import { Router } from "express";
+import { Request, Response, response, Router } from "express";
+import PdfPrinter from "pdfmake";
+import { TDocumentDefinitions } from "pdfmake/interfaces"; // eslint-disable-line import/no-unresolved
 
+import { ProductsRepository } from "../../../../modules/products/infra/typeorm/repositories/ProductsRepository";
 import { CreateProductController } from "../../../../modules/products/useCases/createProduct/CreateProductController";
 import { ListProductsController } from "../../../../modules/products/useCases/listProducts/ListProductController";
 
@@ -11,5 +14,54 @@ const listProductsController = new ListProductsController();
 productsRoutes.post("/", createProductController.handle);
 
 productsRoutes.get("/", listProductsController.handle);
+
+productsRoutes.get("/report", async (request: Request, response: Response) => {
+  const productsRepository = new ProductsRepository();
+  const products = (await productsRepository.list()).map((product) => {
+    return [
+      product.id,
+      product.description,
+      product.price.toString(),
+      product.amount.toString(),
+    ];
+  });
+
+  console.log(products);
+
+  const fonts = {
+    Helvetica: {
+      normal: "Helvetica",
+      bold: "Helvetica-Bold",
+      italics: "Helvetica-Oblique",
+      bolditalics: "Helvetica-BoldOblique",
+    },
+  };
+
+  const printer = new PdfPrinter(fonts);
+
+  const docDefinitions: TDocumentDefinitions = {
+    defaultStyle: { font: "Helvetica" },
+    content: [{ text: "Meu Primeiro Relatório." }],
+  };
+
+  const pdfDoc = printer.createPdfKitDocument(docDefinitions);
+
+  const chunks = [];
+
+  pdfDoc.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  pdfDoc.end();
+
+  pdfDoc.on("end", () => {
+    console.log("Aqui");
+  });
+
+  pdfDoc.on("end", () => {
+    const result = Buffer.concat(chunks);
+    return response.end(result);
+  });
+});
 
 export { productsRoutes };
